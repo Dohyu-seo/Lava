463e6401b103b3882351841349ce08f8f9caaabf
class02 <class02@DESKTOP-BA8JVKU>
2020-03-26 19:18:04 +0900

    1

--- Java00/src/day14/SamgakTest.java
+++ Java00/src/day14/SamgakTest.java
@@ -3,7 +3,7 @@ package day14;
 import javax.swing.JOptionPane;
 
 public class SamgakTest {
-
+	Samgak s1, s2;
 	public SamgakTest() throws NumberFormatException {
 
 		// JOptionpane로 입력받기~
@@ -13,20 +13,20 @@ public class SamgakTest {
 		String s2num2 = JOptionPane.showInputDialog("두 번째 삼각형의  높이를 입력하세요.");
 		
 		try {
-		// 입력받은 값 변수에 담기~
-		int s1Base = Integer.parseInt(s1num);
-		int s1Height = Integer.parseInt(s1num2);
-		int s2Base = Integer.parseInt(s2num);
-		int s2Height = Integer.parseInt(s2num2);
-		
-		// 비교할 삼각형 두개 만들기~
-		Samgak s1 = new Samgak(s1Base, s1Height);
-		Samgak s2 = new Samgak(s2Base, s2Height);
-		
-		// private으로 설정 되어 있기 때문에 toString으로 꺼내오자~
-		JOptionPane.showMessageDialog(null,
-				(s1.toString() + s2.toString() + "두 삼각형은 " + 
-						((s1.equals(s2)) ? ("같은 삼각형 입니다.") : ("다른 삼각형 입니다."))));
+			// 입력받은 값 변수에 담기~
+			int s1Base = Integer.parseInt(s1num);
+			int s1Height = Integer.parseInt(s1num2);
+			int s2Base = Integer.parseInt(s2num);
+			int s2Height = Integer.parseInt(s2num2);
+			
+			// 비교할 삼각형 두개 만들기~
+			s1 = new Samgak(s1Base, s1Height);
+			s2 = new Samgak(s2Base, s2Height);
+			
+			// private으로 설정 되어 있기 때문에 toString으로 꺼내오자~
+			JOptionPane.showMessageDialog(null,
+					(s1.toString() + s2.toString() + "두 삼각형은 " + 
+							((s1.equals(s2)) ? ("같은 삼각형 입니다.") : ("다른 삼각형 입니다."))));
 		} catch(NumberFormatException e) {
 			JOptionPane.showMessageDialog(null, "정수를 입력해 주세요~");
 		}
